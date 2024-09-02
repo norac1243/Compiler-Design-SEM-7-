a8059190517f9713f9426e0d11566454fcffe3ab
norac1243 <phicsp1234@gmail.com>
2024-09-03 00:14:26 +0530

    can add multiple lines of code as i/p. added more keywords and operators. fixed the count problem that persisted for identifiers and keywrds

--- lexical_analyzer.cpp
+++ lexical_analyzer.cpp
@@ -37,61 +37,67 @@ void feed_in_table(string str, int count, string sym_type)
 		m++;
 	}
 	if (sym_type == "keyword")
-		cout << sym_table[index][2] << sym_table[index][0] << ">";
+		cout << "<" << sym_table[index][2] << "," << sym_table[index][0] << ">";
 	if (sym_type == "op" || sym_type == "int")
 		cout << "<" << sym_table[index][0] << ">";
 	if (sym_type == "id")
 		cout << "<id" << sym_table[index][1] << "," << sym_table[index][0] << ">";
-
-	// print_token(index);
+	if (sym_table[index][0] == ")" || sym_table[index][0] == "}" || sym_table[index][0] == ";")
+		cout << endl;
 }
 
 int is_operator_func(char input)
 {
-	char operator_arr[20] = {'+', '-', '/', '*', '=', '>', '<'};
-	int flag = 0;
-	for (int j = 0; j < 5; j++)
+	char operator_arr[25] = {'+', '-', '/', '*', '=', '>', '<', '!', '&', '|', '^', ',', '.', ':', '"', '%', '~', '}', '{', '(', ')', ';'};
+	for (char op : operator_arr)
 	{
-		if (input == operator_arr[j]) // checks for operator
-		{
-			flag = 1;
-			break;
-		}
+		if (input == op) // checks for operator
+			return 1;
 	}
-	return flag;
+	return 0;
 }
 
 int is_keyword_func(string input)
 {
-	string keyword_arr[900] = {"if", "else", "else if", "int", "float", "double"};
-	int flag = 0;
-	for (int j = 0; j < 5; j++)
+	string keyword_arr[900] = {"auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else",
+							   "enum", "extern", "float", "for", "goto", "if", "int", "long", "register", "return",
+							   "short", "signed", "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned",
+							   "void", "volatile", "while", "alignas", "alignof", "and", "and_eq", "asm", "bitand", "bitor",
+							   "class", "compl", "concept", "const_cast", "delete", "dynamic_cast", "explicit", "export",
+							   "friend", "inline", "mutable", "namespace", "new", "noexcept", "not", "not_eq", "nullptr",
+							   "operator", "private", "protected", "public", "reinterpret_cast", "static_assert", "static_cast",
+							   "template", "this", "thread_local", "throw", "try", "typeid", "typename", "using", "virtual",
+							   "wchar_t", "xor", "xor_eq"};
+	for (string key : keyword_arr)
 	{
-		if (input == keyword_arr[j]) // checks for operator
-		{
-			flag = 1;
-			break;
-		}
+		if (input == key) // checks for operator
+			return 1;
 	}
-	return flag;
+	return 0;
 }
 
 int main()
 {
-	string string_input, check_string, string_passed;
-	int op_count = 1, id_count = 1, next;
+	string string_input, check_string, string_passed, input_line;
+	int op_count = 1, id_count = 1, keyword_count = 1, next;
 	// is_op = 0, is_alphabet = 0,
 	cout << setw(15) << "LEXICAL ANALYSER FOR" << endl
 		 << "-------------------  C LANGUAGE -------------------" << endl
-		 << "enter expression:" << endl;
-	getline(cin, string_input);
+		 << "Enter Code, type 'EOF' to stop:" << endl;
+	// getline(cin, string_input);
+	while (getline(cin, input_line) && input_line != "EOF")
+	{
+		string_input += input_line + " ";
+	}
 	int n = string_input.length();
+
+	cout << endl
+		 << "LEXICAL ANALYSIS PHASE:" << endl;
 	for (int i = 0; i < n; i++)
 	{
 		if (is_operator_func(string_input[i]) == 1) // operator
 		{
 			string_passed = string_input[i];
-			// check if increment or decrement operator
 			next = i + 1;
 			if (string_input[next] == string_input[i]) // is increment or decrement or double equal for checking stuff in an if loop
 			{
@@ -99,10 +105,9 @@ int main()
 				i++; // skips string_input[next]
 			}
 			feed_in_table(string_passed, op_count, "op");
-			// cout << string_passed << " -> op" << op_count << endl;
 			op_count++;
 		}
-		else if (isalpha(string_input[i])) // an alp
+		else if (isalpha(string_input[i])) // an identifer or keyword
 		{
 			string_passed = "";
 			int next_is_alphabet = 0;
@@ -121,20 +126,27 @@ int main()
 					next_is_alphabet = 0;
 			} while (next_is_alphabet == 1);
 			if (is_keyword_func(string_passed) == 1)
-				feed_in_table(string_passed, id_count, "keyword");
+			{
+				feed_in_table(string_passed, keyword_count, "keyword");
+				keyword_count++;
+			}
 			else
+			{
 				feed_in_table(string_passed, id_count, "id");
-			// cout << " -> id" << id_count << endl;
-			id_count++;
+				id_count++;
+			}
 		}
-		else if (string_input[i] == ' ') //
+		else if (string_input[i] == ' ') // to handle whitespaces.
 		{
 			continue;
 		}
 	}
+	cout << endl
+		 << "SYMBOL TABLE: \n --------------------";
 	for (int i = 0; i < m; i++)
 	{
-		cout << sym_table[i][0] << " | count" << sym_table[i][1] << " | " << sym_table[i][2] << endl;
+		cout << endl
+			 << setw(10) << sym_table[i][0] << " | " << setw(10) << sym_table[i][2] << sym_table[i][1] << " | ";
 	}
 	_getch();
 	return 0;
