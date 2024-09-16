cfe0d3d0fb6b57d239957cc2d567ac45a22d9f1c
norac1243 <phicsp1234@gmail.com>
2024-09-16 20:07:45 +0530

    Fixed header files identification as keyword

--- lexical_analyzer.cpp
+++ lexical_analyzer.cpp
@@ -26,7 +26,7 @@ int handleCount(string sym_type)
 	return count;
 }
 
-void print_token(int index, const string& type, const string& str)
+void print_token(int index, const string &type, const string &str)
 {
 	if (type == "keyword")
 		cout << "< " << sym_table[index][2] << "," << sym_table[index][0] << " > ";
@@ -38,7 +38,6 @@ void print_token(int index, const string& type, const string& str)
 	if (str == ";" || str == "}" || str == ")")
 		cout << endl;
 }
-
 
 void feed_in_table(string str, string sym_type)
 {
@@ -62,13 +61,12 @@ void feed_in_table(string str, string sym_type)
 		index = m;
 		m++;
 	}
-
 	print_token(index, sym_type, str);
 }
 
 bool is_operator_func(char input)
 {
-	char operator_arr[30] = { '+', '-', '/', '*', '=', '!', '&', '|', '^', '.', ':', '\"', '%', '~', '[', ']', '>','<',';',',','\'','"','(',')','{','}','#' };
+	char operator_arr[30] = {'+', '-', '/', '*', '=', '!', '&', '|', '^', '.', ':', '\"', '%', '~', '[', ']', '>', '<', ';', ',', '\'', '"', '(', ')', '{', '}', '#'};
 	for (char op : operator_arr)
 	{
 		if (input == op) // checks for operator
@@ -79,7 +77,7 @@ bool is_operator_func(char input)
 
 bool is_relop_func(string input)
 {
-	string rel_operator_arr[] = { "==", "!=", "<=", ">=" };
+	string rel_operator_arr[] = {"==", "!=", "<=", ">="};
 	for (string relop : rel_operator_arr)
 	{
 		if (input == relop) // checks for relational operator
@@ -90,7 +88,7 @@ bool is_relop_func(string input)
 
 bool is_delimiter_func(string input)
 {
-	string delimiter_arr[] = { "/*","*/","//" };
+	string delimiter_arr[] = {"/*", "*/", "//"};
 	for (string delimiter : delimiter_arr)
 	{
 		if (input == delimiter) // checks for operator
@@ -99,10 +97,9 @@ bool is_delimiter_func(string input)
 	return false;
 }
 
-
 bool is_keyword_func(string input)
 {
-	string keyword_arr[120] = { "auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else","enum", "extern", "float", "for", "goto", "if", "int", "long", "register", "return","short", "signed", "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned","void", "volatile", "while", "alignas", "alignof", "and", "and_eq", "asm", "bitand", "bitor","class", "compl", "concept", "const_cast", "delete", "dynamic_cast", "explicit", "export","friend", "inline", "mutable", "namespace", "new", "noexcept", "not", "not_eq", "nullptr","operator", "private", "protected", "public", "reinterpret_cast", "static_assert", "static_cast","template", "this", "thread_local", "throw", "try", "typeid", "typename", "using", "virtual","wchar_t", "xor", "xor_eq", "true", "false", "include","printf","main", "stdio.h", "stdlib.h", "string.h", "math.h", "ctype.h", "time.h", "errno.h", "assert.h", "limits.h", "float.h", "stddef.h", "stdarg.h", "stdbool.h", "signal.h", "setjmp.h", "locale.h", "unistd.h", "sys/types.h", "sys/stat.h", "sys/wait.h", "pthread.h", "windows.h", "io.h"};
+	string keyword_arr[120] = {"auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum", "extern", "float", "for", "goto", "if", "int", "long", "register", "return", "short", "signed", "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while", "alignas", "alignof", "and", "and_eq", "asm", "bitand", "bitor", "class", "compl", "concept", "const_cast", "delete", "dynamic_cast", "explicit", "export", "friend", "inline", "mutable", "namespace", "new", "noexcept", "not", "not_eq", "nullptr", "operator", "private", "protected", "public", "reinterpret_cast", "static_assert", "static_cast", "template", "this", "thread_local", "throw", "try", "typeid", "typename", "using", "virtual", "wchar_t", "xor", "xor_eq", "true", "false", "include", "printf", "main", "stdio.h", "stdlib.h", "string.h", "math.h", "ctype.h", "time.h", "errno.h", "assert.h", "limits.h", "float.h", "stddef.h", "stdarg.h", "stdbool.h", "signal.h", "setjmp.h", "locale.h", "unistd.h", "sys/types.h", "sys/stat.h", "sys/wait.h", "pthread.h", "windows.h", "io.h"};
 	for (string key : keyword_arr)
 	{
 		if (input == key) // checks for operator
@@ -146,9 +143,9 @@ int main()
 	int next;
 	// is_op = 0, is_alphabet = 0,
 	cout << setw(60) << "LEXICAL ANALYSER FOR" << endl
-		<< "------------------------------------------- C LANGUAGE -------------------------------------------" << endl
-		<< "Enter Code, type 'EOF' to stop:" << endl
-		<< "--------------------------------------------------------------------------------------------------" << endl;
+		 << "------------------------------------------- C LANGUAGE -------------------------------------------" << endl
+		 << "Enter Code, type 'EOF' to stop:" << endl
+		 << "--------------------------------------------------------------------------------------------------" << endl;
 	// getline(cin, string_input);
 	while (getline(cin, input_line) && input_line != "EOF")
 	{
@@ -157,8 +154,8 @@ int main()
 	int n = string_input.length();
 
 	cout << "--------------------------------------------------------------------------------------------------" << endl
-		<< "LEXICAL ANALYSIS PHASE (TOKENS):" << endl
-		<< endl;
+		 << "LEXICAL ANALYSIS PHASE (TOKENS):" << endl
+		 << endl;
 	for (int i = 0; i < n; i++)
 	{
 		if (is_operator_func(string_input[i])) // operators and relational operator
@@ -167,34 +164,36 @@ int main()
 			next = i + 1;
 			int is_relop = 0, is_delimiter = 0;
 
-			//increment decrement checker 
+			// increment decrement checker
 			if (next < n && (string_input[next] == string_input[i]) &&
 				(string_input[i] == '+' || string_input[i] == '-')) // is increment or decrement or double equal for checking stuff in an if loop
 			{
 				string_passed += string_input[next];
 				i++; // skips string_input[next]
 			}
-			//relational operator cgecker 
+			// relational operator cgecker
 			else if (next < n && (string_input[i] == '>' || string_input[i] == '<' || string_input[i] == '!' || string_input[i] == '='))
 			{
 				newstring = string_passed + string_input[next];
 				is_relop = 1;
 				if (is_relop_func(newstring))
 				{
-					i++;  string_passed = newstring;
+					i++;
+					string_passed = newstring;
 				}
 			}
-			//delimitter
+			// delimitter
 			else if (next < n && (string_input[i] == ';' || string_input[i] == ',' || string_input[i] == '\'' || string_input[i] == '"' || string_input[i] == '(' || string_input[i] == ')' || string_input[i] == '{' || string_input[i] == '}' || string_input[i] == '/' || string_input[i] == '*'))
 			{
 				newstring = string_passed + string_input[next];
 				if (is_delimiter_func(newstring))
 				{
-					i++;  string_passed = newstring;
+					i++;
+					string_passed = newstring;
 				}
 				is_delimiter = 1;
 			}
-			//equal and 1 throwing exception and hence
+			// equal and 1 throwing exception and hence
 			if (string_passed == "=" || string_passed == "!")
 			{
 				feed_in_table(string_passed, "op");
@@ -225,7 +224,7 @@ int main()
 			if (is_float(string_passed))
 				feed_in_table(string_passed, "float");
 			else if (is_integer(string_passed))
-				feed_in_table(string_passed,"int");
+				feed_in_table(string_passed, "int");
 			else
 				cout << "<Invalid number literal: " << string_passed << ">";
 		}
@@ -239,7 +238,7 @@ int main()
 				string_passed += string_input[i];
 				k++;
 				next = i + 1;
-				if (isalpha(string_input[next]))
+				if (isalpha(string_input[next]) || (string_input[next] == '.' && isalpha(string_input[next + 1])))
 				{
 					next_is_alphabet = 1;
 					i++;
@@ -249,11 +248,11 @@ int main()
 			} while (next_is_alphabet == 1);
 			if (is_keyword_func(string_passed))
 			{
-				feed_in_table(string_passed,"keyword");
+				feed_in_table(string_passed, "keyword");
 			}
 			else
 			{
-				feed_in_table(string_passed,"id");
+				feed_in_table(string_passed, "id");
 			}
 		}
 		else if (string_input[i] == ' ') // to handle whitespaces.
@@ -262,33 +261,33 @@ int main()
 		}
 	}
 	cout << "--------------------------------------------------------------------------------------------------" << endl
-		<< "SYMBOL TABLE:" << endl
-		<< "------------------------------------------" << endl
-		<< setw(15) << "Name" << " | " << setw(22) << "Type" << " |" << endl
-		<< "------------------------------------------";
+		 << "SYMBOL TABLE:" << endl
+		 << "------------------------------------------" << endl
+		 << setw(15) << "Name" << " | " << setw(22) << "Type" << " |" << endl
+		 << "------------------------------------------";
 
 	for (int i = 0; i < m; i++)
 	{
 		if (sym_table[i][2] == "int")
 		{
 			cout << endl
-				<< setw(15) << sym_table[i][0] << " | " << setw(15) << "literal " << sym_table[i][1] << " (" << sym_table[i][2] << ")" << " |";
+				 << setw(15) << sym_table[i][0] << " | " << setw(15) << "literal " << sym_table[i][1] << " (" << sym_table[i][2] << ")" << " |";
 		}
 		else if (sym_table[i][2] == "float")
 		{
 			{
 				cout << endl
-					<< setw(15) << sym_table[i][0] << " | " << setw(13) << "literal " << sym_table[i][1] << " (" << sym_table[i][2] << ")" << " |";
+					 << setw(15) << sym_table[i][0] << " | " << setw(13) << "literal " << sym_table[i][1] << " (" << sym_table[i][2] << ")" << " |";
 			}
 		}
 		else
 		{
 			cout << endl
-				<< setw(15) << sym_table[i][0] << " | " << setw(20) << sym_table[i][2] << " " << sym_table[i][1] << " |";
+				 << setw(15) << sym_table[i][0] << " | " << setw(20) << sym_table[i][2] << " " << sym_table[i][1] << " |";
 		}
 	}
 	cout << endl
-		<< "--------------------------------------------------------------------------------------------------" << endl;
+		 << "--------------------------------------------------------------------------------------------------" << endl;
 	_getch();
 	return 0;
 }
